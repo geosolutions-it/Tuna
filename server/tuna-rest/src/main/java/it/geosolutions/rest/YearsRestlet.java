d86457e596e4359bf9b3c6f106146fd34121d80a
tobia <tobia.dipisa@geo-solutions.it>
2011-10-25 10:03:45 +0200

    Introducing some doc

--- server/tuna-rest/src/main/java/it/geosolutions/rest/YearsRestlet.java
+++ server/tuna-rest/src/main/java/it/geosolutions/rest/YearsRestlet.java
@@ -1,3 +1,22 @@
+/*
+ *  Copyright (C) 2007 - 2011 GeoSolutions S.A.S.
+ *  http://www.geo-solutions.it
+ *
+ *  GPLv3 + Classpath exception
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package it.geosolutions.rest;
 
 import it.geosolutions.dao.YearsDAO;
@@ -18,6 +37,12 @@ import org.restlet.data.Request;
 import org.restlet.data.Response;
 import org.restlet.data.Status;
 
+/**
+ * YearsRestlet class.
+ * 
+ * @author Tobia Di Pisa
+ *
+ */
 public class YearsRestlet extends Restlet{
 
 	private final static Logger LOGGER = Logger.getLogger(YearsRestlet.class.toString());
